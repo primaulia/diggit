923d558e03f11c8b13c01ab8a17688aec7566583
Yishu See <yishuyishu@gmail.com>
2017-07-09 02:19:14 +0800

    Add React build path to serve as static files on Express

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.use('/api/v1', index);
 
