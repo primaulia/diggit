40b04ec3bd3c119d4d031381fdd70621fab11021
Yishu See <yishuyishu@gmail.com>
2017-07-09 02:16:42 +0800

    Add topics api routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var topicController = require('../controllers/topicController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Hello World!')
-});
+router.get('/topics', topicController.getTopics);
+
+router.post('/topics', topicController.addTopic)
+
+router.patch('/topics/:id', topicController.updownvote)
 
 module.exports = router;
