b13c1216f7f1a942ae90a36862c6631efb969b41
Yishu See <yishuyishu@gmail.com>
2017-07-09 02:23:18 +0800

    Handle on value change in texture in CreateTopic component

--- src/CreateTopic.js
+++ src/CreateTopic.js
@@ -7,6 +7,23 @@ class CreateTopic extends Component {
     this.state = {
       value: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleCreate() {
+    this.props.onCreate(this.state.value)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.loading) {
+      this.setState({value: ''})
+    }
   }
 
   render() {
@@ -17,10 +34,12 @@ class CreateTopic extends Component {
           rows={4}
           my={2}
           placeholder="What's up?"
+          onChange={this.handleChange}
           value={this.state.value}
         />
         <ButtonOutline
           children={loading ? 'Loading...' : 'Post a new topic'}
+          onClick={this.handleCreate}
           disabled={loading || this.state.value === ''}
         />
       </div>
