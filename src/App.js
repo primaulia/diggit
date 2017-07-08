5318f539ce6d6e244fc0976a8709bd3279fe8bcd
Yishu See <yishuyishu@gmail.com>
2017-07-09 02:26:04 +0800

    Add children components to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,103 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { topicsUrl, topicUrl } from './helpers/api'
+import { Provider, Container, Heading } from 'rebass'
+import CreateTopic from './CreateTopic'
+import TopicsList from './TopicsList'
+import Pagination from './Pagination'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      topics: [],
+      loading: false,
+      currentPage: 1
+    }
+
+    this.setTopics = this.setTopics.bind(this)
+    this.handleVote = this.handleVote.bind(this)
+    this.handleCreateTopic  = this.handleCreateTopic.bind(this)
+    this.handleTopicsListUpdate = this.handleTopicsListUpdate.bind(this)
+    this.handlePageClick = this.handlePageClick.bind(this)
+  }
+
+  getTopics(page) {
+    fetch(topicsUrl + "?page=" + page)
+    .then(response => response.json())
+    .then(this.setTopics)
+  }
+
+  setTopics(res) {
+    this.setState({
+      topics: res.data.topics,
+      pages: res.data.pages
+    })
+  }
+
+  handleVote(topicId, count) {
+    fetch(topicUrl(topicId), {
+      method: 'PATCH',
+      body: JSON.stringify({"votes": count}),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      })
+    })
+  }
+
+  handleCreateTopic(content) {
+    this.setState({ loading: true })
+    let topics = this.state.topics
+    let newTopic = {
+      content,
+      id: topics.length + 1,
+      votes: 0
+    }
+    topics.unshift(newTopic)
+    this.setState({ topics })
+    fetch(topicsUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        'content': content
+      }),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      })
+    })
+    .then(response => response.json())
+    .then(this.handleTopicsListUpdate)
+  }
+
+  handleTopicsListUpdate(res) {
+    this.setState({
+      loading: false,
+      topics: res.data.topics,
+      pages: res.data.pages
+    })
+  }
+
+  handlePageClick(page) {
+    this.setState({ currentPage: page })
+    this.getTopics(page)
+  }
+
+  componentDidMount() {
+    this.getTopics(1)
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <Provider>
+        <Container m={5}>
+          <Heading children='Diggit' />
+          <CreateTopic onCreate={this.handleCreateTopic} loading={this.state.loading}/>
+          <TopicsList topics={this.state.topics} onVote={this.handleVote} />
+          <Pagination count={this.state.pages} current={this.state.currentPage} onPageClick={this.handlePageClick}/>
+        </Container>
+      </Provider>
     );
   }
 }
