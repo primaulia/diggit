238fbcf237c621112653c9eba9522174f0024fa6
Yishu See <yishuyishu@gmail.com>
2017-07-09 02:17:20 +0800

    Add route to api

--- server/app.js
+++ server/app.js
@@ -14,7 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/api/v1', index);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
