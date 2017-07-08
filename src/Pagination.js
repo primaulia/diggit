4a44a2d5981309e98920cf12b76799dfb83d2edb
Yishu See <yishuyishu@gmail.com>
2017-07-09 02:24:53 +0800

    Add Pagination component

---
+++ src/Pagination.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Group, Flex, ButtonOutline, Button } from 'rebass'
+
+class Pagination extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      current: props.current
+    }
+
+    this.handlePageClick = this.handlePageClick.bind(this)
+  }
+
+  handlePageClick(i) {
+    this.setState({ current: i })
+    this.props.onPageClick(i)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ current: nextProps.current })
+  }
+
+  render() {
+    const { count } = this.props
+    const listItems = []
+    for (let i = 1; i <= count; i++) {
+      if (i === this.state.current) {
+        listItems.push( <Button children={i} key={i} /> )
+      } else {
+        listItems.push( <ButtonOutline children={i} key={i} onClick={this.handlePageClick.bind(this, i)}/> )
+      }
+    }
+    return (
+      <Flex justify="center">
+        <Group>
+          {listItems}
+        </Group>
+      </Flex>
+    )
+  }
+}
+
+export default Pagination
